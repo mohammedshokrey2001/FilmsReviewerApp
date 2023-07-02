eaab058043c8cb70dd0d15522530a12897c8c654
mohamed shokry <mohamedhunter6@gmail.com>
2023-07-02 05:00:41 +0300

    update the YoutubeActivity.kt adding the OwnVideoPlayer to it

--- app/src/main/java/com/example/myapplication_caching/ui/screens/players/youtube/YoutubeActivity.kt
+++ app/src/main/java/com/example/myapplication_caching/ui/screens/players/youtube/YoutubeActivity.kt
@@ -2,20 +2,26 @@ package com.example.myapplication_caching.ui.screens.players.youtube
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.myapplication_caching.BuildConfig
 import com.example.myapplication_caching.R
 import com.example.myapplication_caching.databinding.ActivityYoutubeBinding
 import com.example.myapplication_caching.ui.screens.players.base.factory.PlayerFactory
+import com.example.myapplication_caching.ui.screens.players.player_interface.OwnVideoPlayer
 import com.example.myapplication_caching.utilites.Utilities
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 
 
-class YoutubeActivity :  YouTubeBaseActivity(),YouTubePlayer.OnInitializedListener {
+class YoutubeActivity :  YouTubeBaseActivity(),YouTubePlayer.OnInitializedListener , OwnVideoPlayer{
 
     private lateinit var binding:ActivityYoutubeBinding
     private lateinit var filmId :String
+
+    private  var player:YouTubePlayer? = null
+    private val youtubeKey = BuildConfig.YOUTUBE_KEY
 
    private val TAG = "YoutubeActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +31,27 @@ class YoutubeActivity :  YouTubeBaseActivity(),YouTubePlayer.OnInitializedListen
         Log.i(TAG, "onCreate: youtube")
         binding = DataBindingUtil.setContentView(this,R.layout.activity_youtube)
 
-            filmId = intent.getStringExtra(PlayerFactory.URL).toString()
-
-        binding.youtubePlayerView.initialize(Utilities.YOUTUBE_KEY, this)
 
 
+        initialize(filmId)
+
+    }
+    override fun initialize(url: String) {
+        filmId = intent.getStringExtra(PlayerFactory.URL).toString()
+        binding.youtubePlayerView.initialize(youtubeKey, this)
+
+    }
+
+
+    override fun stop() {
+        player?.pause()
+        Toast.makeText(this, "cant play you video", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun play() {
+        player?.play()
+        Log.i(TAG, "onInitializationSuccess: done")
 
     }
 
@@ -38,18 +60,23 @@ class YoutubeActivity :  YouTubeBaseActivity(),YouTubePlayer.OnInitializedListen
         p1: YouTubePlayer?,
         p2: Boolean
     ) {
+        player = p1
         p1?.loadVideo(filmId)
-        p1?.play()
+        play()
     }
 
     override fun onInitializationFailure(
         p0: YouTubePlayer.Provider?,
         p1: YouTubeInitializationResult?
     ) {
-        TODO("Not yet implemented")
+        Log.i(TAG, "onInitializationFailure:failed ${p1.toString()}")
     }
 
 
+    override fun onStop() {
+        super.onStop()
+        stop()
+    }
 }
 
 
--- app/src/main/java/com/example/myapplication_caching/utilites/Utilities.kt
+++ app/src/main/java/com/example/myapplication_caching/utilites/Utilities.kt
@@ -7,10 +7,6 @@ object Utilities {
     //second api
     const val BASE_URL_MINI_MOVIE = "https://moviesminidatabase.p.rapidapi.com/"
 
-
-    const val YOUTUBE_KEY = "AIzaSyAqRp8H0Qg2bW8vytGPcbaoVTVyB2u5wus"
-
-    const val API_KEY = "d7132d47f83deb0b3369582fd105134e"
      const val LANGUAGE = "en-US"
 
 
